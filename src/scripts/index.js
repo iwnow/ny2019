ef59622b964a7a9c43ebce6452095864ddc25dba
iwnow <vostroknutov@hotmail.com>
2018-12-13 02:23:42 +0300

    +

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,8 +7,15 @@ import {api} from './api';
 
     function start({ userName }) {
         $('#userName').text((userName || '').toUpperCase());
+        $(document.body).addClass('loaded');
+
         setTimeout(() => {
-            $(document.body).addClass('loaded');
-        }, 500);
+            $('.label-wrapper-1').animate({
+                opacity: 0,
+              }, 500, function() {
+                $('.label-wrapper-1').css('display', 'none');
+                $(document.body).addClass('second');
+              });
+        }, 2000);
     }
 })();
