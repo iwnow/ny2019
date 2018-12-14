5b1eeffeb1f44a15e7a9ac9aa51d3e09a120b177
iwnow <vostroknutov@hotmail.com>
2018-12-14 04:10:04 +0300

    +

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,14 +8,56 @@ import {api} from './api';
     function start({ userName }) {
         $('#userName').text((userName || '').toUpperCase());
         $(document.body).addClass('loaded');
-
+        scene2to3();
         setTimeout(() => {
             $('.label-wrapper-1').animate({
                 opacity: 0,
               }, 500, function() {
                 $('.label-wrapper-1').css('display', 'none');
                 $(document.body).addClass('second');
+                setTimeout(() => {
+                    $(document.body).addClass('third');
+                }, 6000);
               });
         }, 2000);
+    }
+
+    function scene2to3() {
+        const bg = $('.bg');
+        const width = bg.width();
+        let lineWidth = width / 6;
+        lineWidth = lineWidth < 200 ? 200 : lineWidth;
+        
+        $('.bg3').width(width).height(bg.height());
+        
+        const edgeLeft = (width / 2) - (lineWidth/2);
+        const edgeRight = (width / 2) + (lineWidth/2);
+        // $('.bg4').width(lineWidth);
+        let lines = [];
+        let j = 1;
+        for (let i = 7; i > 0; i--) {
+            let even = i % 2 === 0;
+            lines.push(
+                $(`<div class="bg4 line${i} ${even ? 'even' : 'odd'}"></div>`)
+                    .width(lineWidth)
+                    .css('left', `${edgeLeft - j*lineWidth + j}px`)
+            );
+            j++;
+        }
+        lines.push(
+            $(`<div class="bg4 line8 even"></div>`)
+                .width(lineWidth)
+        );
+        j = 0;
+        for (let i = 9; i < 16; i++) {
+            let even = i % 2 === 0;
+            lines.push(
+                $(`<div class="bg4 line${i} ${even ? 'even' : 'odd'}"></div>`)
+                    .width(lineWidth)
+                    .css('left', `${edgeRight + j*lineWidth - j - 1}px`)
+            );
+            j++;
+        }
+        $('.first-screen').append(lines);
     }
 })();
